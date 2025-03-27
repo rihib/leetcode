165bfe64f9460f01bceb2e1ae1b3dc4fc650b97a
rihib <rihib@rihib.dev>
2025-03-27 21:32:39 +0900

    refactor search in rotated sorted array

--- go/search_in_rotated_sorted_array.go
+++ go/search_in_rotated_sorted_array.go
@@ -25,21 +25,22 @@ func searchClosed(nums []int, target int) int {
 	return -1
 }
 
+// 半閉区画ではleft~right-1が未探索で、rightは常に探索済み
 func searchHalfClosed(nums []int, target int) int {
 	left, right := 0, len(nums)
 	for left < right {
 		mid := left + (right-left)/2
-		if target == nums[mid] {
+		if nums[mid] == target {
 			return mid
 		}
-		if nums[left] <= nums[mid] {
-			if nums[left] <= target && target < nums[mid] {
+		if nums[left] < nums[mid] { // この問題の条件では全ての要素が異なるので、nums[left] == nums[mid]の時点で-1を返せばいいので、<=とする必要はない
+			if nums[left] <= target && target < nums[mid] { // leftがtargetの可能性もあるので<=とする
 				right = mid
 			} else {
 				left = mid + 1
 			}
 		} else {
-			if nums[mid] < target && target <= nums[right-1] {
+			if nums[mid] < target && target <= nums[right-1] { // rightは最初は範囲外なので-1をする。その場合right-1は未探索なので<=とする
 				left = mid + 1
 			} else {
 				right = mid
