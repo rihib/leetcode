230ac42b2e16436b4d40510f2c3760de3eb91ce9
rihib <rihib@rihib.dev>
2025-04-03 00:44:17 +0900

    add comment

--- go/search_insert_position.go
+++ go/search_insert_position.go
@@ -13,3 +13,9 @@ func searchInsert(nums []int, target int) int {
 	}
 	return left
 }
+
+// forループを抜けるのは探索範囲の長さが１以下になったとき
+// 長さ２のnums={1, 2}があるとき、
+// left=0, right=2, mid=1になる。
+// target=3のとき、ループを抜ける直前にleft=mid+1されるので、ループを抜けたらleftを返せばいい。
+// target=0のとき、leftはそのままなので、ループを抜けたらleftを返せばいい
