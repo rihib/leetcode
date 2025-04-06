cdf14295a069219e3dac0005ab26cdec8667069b
rihib <rihib@rihib.dev>
2025-04-06 09:25:53 +0900

    refactor combination sum

--- go/combination_sum.go
+++ go/combination_sum.go
@@ -3,9 +3,8 @@ package main
 
 import "slices"
 
-// 時間計算量： 両者とも最悪で指数だが、バックトラッキングの方が剪定の効果があり実行時間は短くなりやすい。
-
-// 空間計算量： DPはすべての部分和の組み合わせを記録するため重い。バックトラッキングは再帰の深さ分だけを保持するため軽い。
+// 時間計算量： 両者とも最悪で指数時間かかるが、バックトラッキングの方が剪定の効果があり実行時間は短くなりやすい。
+// 空間計算量： DPはすべての部分和の組み合わせを記録するためメモリ使用量が多くなる。バックトラッキングは再帰の深さ分だけを保持するためメモリ使用量は少なくなる。
 
 func combinationSumBacktrackingRecursion(candidates []int, target int) [][]int {
 	var combinations [][]int
@@ -32,11 +31,11 @@ func combinationSumBacktrackingRecursion(candidates []int, target int) [][]int {
 
 type frame struct {
 	combination  []int
-	currentIndex int
 	sum          int
+	currentIndex int
 }
 
-func combinationSumacktrackingIterative(candidates []int, target int) [][]int {
+func combinationSum(candidates []int, target int) [][]int {
 	var combinations [][]int
 	stack := []frame{{[]int{}, 0, 0}}
 	for len(stack) > 0 {
@@ -50,25 +49,25 @@ func combinationSumacktrackingIterative(candidates []int, target int) [][]int {
 			continue
 		}
 		for i := f.currentIndex; i < len(candidates); i++ {
-			newCombination := slices.Clone(f.combination)
-			newCombination = append(newCombination, candidates[i])
-			stack = append(stack, frame{newCombination, i, f.sum + candidates[i]})
+			combination := slices.Clone(f.combination)
+			combination = append(combination, candidates[i])
+			stack = append(stack, frame{combination, f.sum + candidates[i], i})
 		}
 	}
 	return combinations
 }
 
 func combinationSumDP(candidates []int, target int) [][]int {
-	combinationsGroup := make([][][]int, target+1)
-	combinationsGroup[0] = [][]int{{}}
+	memo := make([][][]int, target+1)
+	memo[0] = [][]int{{}}
 	for _, candidate := range candidates {
-		for i := candidate; i <= target; i++ {
-			for _, combination := range combinationsGroup[i-candidate] {
+		for sum := candidate; sum <= target; sum++ {
+			for _, combination := range memo[sum-candidate] {
 				newCombination := slices.Clone(combination)
 				newCombination = append(newCombination, candidate)
-				combinationsGroup[i] = append(combinationsGroup[i], newCombination)
+				memo[sum] = append(memo[sum], newCombination)
 			}
 		}
 	}
-	return combinationsGroup[target]
+	return memo[target]
 }
