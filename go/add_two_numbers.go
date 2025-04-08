0fb0c16ce16527753a35aed7ebb7c91e2107d976
rihib <rihib@rihib.dev>
2025-04-09 00:31:47 +0900

    refactor add two numbers

--- go/add_two_numbers.go
+++ go/add_two_numbers.go
@@ -32,16 +32,16 @@ func addTwoNumbersHelper(l1 *ListNode, l2 *ListNode, carry int) *ListNode {
 	if l1 == nil && l2 == nil && carry == 0 {
 		return nil
 	}
-	l1Val, l2Val := 0, 0
+	sum := carry
 	if l1 != nil {
-		l1Val = l1.Val
+		sum += l1.Val
 		l1 = l1.Next
 	}
 	if l2 != nil {
-		l2Val = l2.Val
+		sum += l2.Val
 		l2 = l2.Next
 	}
-	sum := l1Val + l2Val + carry
-	head := &ListNode{Val: sum % 10, Next: addTwoNumbersHelper(l1, l2, sum/10)}
-	return head
+	next := addTwoNumbersHelper(l1, l2, sum/10)
+	node := &ListNode{sum % 10, next}
+	return node
 }
