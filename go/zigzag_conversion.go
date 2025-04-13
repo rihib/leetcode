48b5617b7171436e0ae7492aeb3e2c7ddd2eb6ff
rihib <rihib@rihib.dev>
2025-04-13 11:57:29 +0900

    refactor zigzag conversion

--- go/zigzag_conversion.go
+++ go/zigzag_conversion.go
@@ -6,10 +6,10 @@ import "strings"
 // ジグザグの行ごとに作成していき、最後に１つにする
 // sの先頭から見ていくので、その文字の行番号さえわかればその行に追加していくだけ
 func convert(s string, numRows int) string {
-	if numRows == 1 {
+	if numRows <= 1 {
 		return s
 	}
-	convertedRows := make([]strings.Builder, numRows)
+	rows := make([]strings.Builder, numRows)
 	blockSize := numRows*2 - 2
 	for i, r := range s {
 		offset := i % blockSize
@@ -17,12 +17,12 @@ func convert(s string, numRows int) string {
 		if offset < numRows {
 			rowIndex = offset
 		} else {
-			rowIndex = numRows - 2 - (offset - numRows)
+			rowIndex = blockSize - offset
 		}
-		convertedRows[rowIndex].WriteRune(r)
+		rows[rowIndex].WriteRune(r)
 	}
 	var converted strings.Builder
-	for _, row := range convertedRows {
+	for _, row := range rows {
 		converted.WriteString(row.String())
 	}
 	return converted.String()
