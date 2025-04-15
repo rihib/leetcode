5809d17fc47d0a030793193cb03e1b753459cd84
rihib <rihib@rihib.dev>
2025-04-15 17:37:54 +0900

    refactor generate parentheses

--- go/generate_parentheses.go
+++ go/generate_parentheses.go
@@ -5,53 +5,53 @@ import "slices"
 
 func generateParenthesisRecursive(n int) []string {
 	var combinations []string
-	currentCombination := make([]byte, 0, n*2)
+	var combination []rune
 	var generate func(int, int)
-	generate = func(numLeft, numRight int) {
-		if numLeft == n && numRight == n {
-			combinations = append(combinations, string(currentCombination))
+	generate = func(open, close int) {
+		if open == n && close == n {
+			combinations = append(combinations, string(combination))
 			return
 		}
-		if numLeft < n {
-			currentCombination = append(currentCombination, '(')
-			generate(numLeft+1, numRight)
-			currentCombination = currentCombination[:len(currentCombination)-1]
+		if open < n {
+			combination = append(combination, '(')
+			generate(open+1, close)
+			combination = combination[:len(combination)-1]
 		}
-		if numRight < numLeft {
-			currentCombination = append(currentCombination, ')')
-			generate(numLeft, numRight+1)
-			currentCombination = currentCombination[:len(currentCombination)-1]
+		if open > close {
+			combination = append(combination, ')')
+			generate(open, close+1)
+			combination = combination[:len(combination)-1]
 		}
 	}
 	generate(0, 0)
 	return combinations
 }
 
-type combination struct {
-	parenthesis []byte
-	numLeft     int
-	numRight    int
+type frame struct {
+	combination []rune
+	open        int
+	close       int
 }
 
 func generateParenthesisIterative(n int) []string {
 	var combinations []string
-	stack := []combination{{[]byte{}, 0, 0}}
+	stack := []frame{{[]rune{}, 0, 0}}
 	for len(stack) > 0 {
-		current := stack[len(stack)-1]
+		f := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
-		if current.numLeft == n && current.numRight == n {
-			combinations = append(combinations, string(current.parenthesis))
+		if f.open == n && f.close == n {
+			combinations = append(combinations, string(f.combination))
 			continue
 		}
-		if current.numLeft < n {
-			newParenthesis := slices.Clone(current.parenthesis)
-			newParenthesis = append(newParenthesis, '(')
-			stack = append(stack, combination{newParenthesis, current.numLeft + 1, current.numRight})
+		if f.open < n {
+			newCombination := slices.Clone(f.combination)
+			newCombination = append(newCombination, '(')
+			stack = append(stack, frame{newCombination, f.open + 1, f.close})
 		}
-		if current.numRight < current.numLeft {
-			newParenthesis := slices.Clone(current.parenthesis)
-			newParenthesis = append(newParenthesis, ')')
-			stack = append(stack, combination{newParenthesis, current.numLeft, current.numRight + 1})
+		if f.open > f.close {
+			newCombination := slices.Clone(f.combination)
+			newCombination = append(newCombination, ')')
+			stack = append(stack, frame{newCombination, f.open, f.close + 1})
 		}
 	}
 	return combinations
