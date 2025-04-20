34c460f14a6d68b706defa1b7c67eebfe7514609
rihib <rihib@rihib.dev>
2025-04-20 13:59:10 +0900

    refactor search in rotated sorted array

--- go/search_in_rotated_sorted_array.go
+++ go/search_in_rotated_sorted_array.go
@@ -1,35 +1,6 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func searchClosed(nums []int, target int) int {
-	left, right := 0, len(nums)-1
-	for left <= right {
-		mid := left + (right-left)/2
-		if nums[mid] == target {
-			return mid
-		}
-		// ここでは左側がソートされているかを判定しているが
-		// nums[left] < nums[mid]としてしまうと
-		// left == midになるとき（例えばnums=[3, 1]）
-		// 左側がソートされていないと判断されて
-		// 誤った条件分岐に入ってしまう
-		if nums[left] <= nums[mid] {
-			if nums[left] <= target && target < nums[mid] {
-				right = mid - 1
-			} else {
-				left = mid + 1
-			}
-		} else {
-			if nums[mid] < target && target <= nums[right] {
-				left = mid + 1
-			} else {
-				right = mid - 1
-			}
-		}
-	}
-	return -1
-}
-
 // 半閉区画ではleft~right-1が未探索で、rightは常に探索済み
 // そのため、for文の条件はleft < rightとなる（left=1, right=2のときmid=1となり、未探索のleftのみが探索の対象となる）
 func searchHalfClosed(nums []int, target int) int {
@@ -58,6 +29,35 @@ func searchHalfClosed(nums []int, target int) int {
 				left = mid + 1
 			} else {
 				right = mid
+			}
+		}
+	}
+	return -1
+}
+
+func searchClosed(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		// ここでは左側がソートされているかを判定しているが
+		// nums[left] < nums[mid]としてしまうと
+		// left == midになるとき（例えばnums=[3, 1]）
+		// 左側がソートされていないと判断されて
+		// 誤った条件分岐に入ってしまう
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
 			}
 		}
 	}
