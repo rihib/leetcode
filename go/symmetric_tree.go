7c5078d9dc44356f9bb5a047b6a5cf4b1e400ca3
rihib <rihib@rihib.dev>
2025-05-06 17:56:02 +0900

    refactor symmetric tree

--- go/symmetric_tree.go
+++ go/symmetric_tree.go
@@ -3,21 +3,18 @@ package main
 
 import "container/list"
 
-func isSymmetricRecursive(root *TreeNode) bool {
-	return isMirror(root, root)
+func isSymmetric(root *TreeNode) bool {
+	return isSymmetricHelper(root.Left, root.Right)
 }
 
-func isMirror(left, right *TreeNode) bool {
+func isSymmetricHelper(left, right *TreeNode) bool {
 	if left == nil && right == nil {
 		return true
 	}
-	if left == nil || right == nil {
+	if left == nil || right == nil || left.Val != right.Val {
 		return false
 	}
-	if left.Val != right.Val {
-		return false
-	}
-	return isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+	return isSymmetricHelper(left.Left, right.Right) && isSymmetricHelper(left.Right, right.Left)
 }
 
 type nodePair struct {
