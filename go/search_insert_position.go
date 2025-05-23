456db22b8ab04c8d1f857ecc4ee78c29574c0df4
rihib <rihib@rihib.dev>
2025-05-23 18:01:47 +0900

    refactor search insert position

--- go/search_insert_position.go
+++ go/search_insert_position.go
@@ -1,7 +1,12 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-func searchInsert(nums []int, target int) int {
+// 長さ１のnums={1}があるとき、
+// left=0, right=1, mid=0になる。
+// target=2のとき、ループを抜ける直前にleft=mid+1されるので、ループを抜けたらleftを返せばいい。
+// target=0のとき、left=0のままなので、ループを抜けたらleftを返せばい（-1を返すわけではないことに注意。targetはleftよりも小さく、leftの左の値よりも大きいわけなので、leftを返せば良い）
+
+func searchInsertHalfClosed(nums []int, target int) int {
 	left, right := 0, len(nums)
 	for left < right {
 		mid := left + (right-left)/2
@@ -14,7 +19,18 @@ func searchInsert(nums []int, target int) int {
 	return left
 }
 
-// 長さ１のnums={1}があるとき、
-// left=0, right=1, mid=0になる。
-// target=2のとき、ループを抜ける直前にleft=mid+1されるので、ループを抜けたらleftを返せばいい。
-// target=0のとき、left=0のままなので、ループを抜けたらleftを返せばい（-1を返すわけではないことに注意。targetはleftよりも小さく、leftの左の値よりも大きいわけなので、leftを返せば良い）
+func searchInsertClosed(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
