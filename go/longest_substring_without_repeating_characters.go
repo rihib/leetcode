5542a4fa5cbbef02402f0efc8b97c5b4dff7de85
rihib <rihib@rihib.dev>
2025-06-22 14:00:02 +0900

    refactor longest substring without repeating characters

--- go/longest_substring_without_repeating_characters.go
+++ go/longest_substring_without_repeating_characters.go
@@ -1,11 +1,9 @@
 //lint:file-ignore U1000 Ignore all unused code
 package main
 
-// 各文字においてその段階でのSubstringの長さを求め、maxLengthを更新
-// rightは現在の文字のインデックスを表す
-// 常にleftとrightの間には重複が存在しないようにleftを管理する
-// なのでrightを更新した際にleftとrightの間を見て。rightと重複する文字が存在するならその文字を含まないようにleftを更新する
-// そうすることで常にleftとrightの間には重複が存在しないことを保証できるので、rightにおけるSubstringの長さはright-left+1で求めることができる
+// 重複する文字が含まれていない文字列の左端をleft、右端をrightとしたとき、
+// rightが取りうる位置はlen(s)個であり、rightの位置が決まればleftの位置も決まるため、
+// right-left+1を全て比較して最大の長さを求めれば良い。
 func lengthOfLongestSubstring(s string) int {
 	maxLength := 0
 	left := 0
