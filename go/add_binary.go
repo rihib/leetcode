bcb667c476f4086e7c2779244fd5c90409dc689e
rihib <rihib@rihib.dev>
2025-07-06 11:02:12 +0900

    refactor add binary

--- go/add_binary.go
+++ go/add_binary.go
@@ -10,22 +10,22 @@ import (
 func addBinary(a string, b string) string {
 	var reversed strings.Builder
 	carry := 0
-	for i := 1; i <= len(a) || i <= len(b); i++ {
+	for i := 1; ; i++ {
+		if len(a)-i < 0 && len(b)-i < 0 && carry == 0 {
+			break
+		}
 		bitA, bitB := 0, 0
-		if i <= len(a) {
+		if len(a)-i >= 0 {
 			bitA, _ = strconv.Atoi(string(a[len(a)-i]))
 		}
-		if i <= len(b) {
+		if len(b)-i >= 0 {
 			bitB, _ = strconv.Atoi(string(b[len(b)-i]))
 		}
 		sum := bitA + bitB + carry
-		carry = sum / 2
 		reversed.WriteString(strconv.Itoa(sum % 2))
+		carry = sum / 2
 	}
-	if carry == 1 {
-		reversed.WriteByte('1')
-	}
-	result := []rune(reversed.String())
-	slices.Reverse(result)
-	return string(result)
+	added := []rune(reversed.String())
+	slices.Reverse(added)
+	return string(added)
 }
