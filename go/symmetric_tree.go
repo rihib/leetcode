44139106a39e37b27ec8510ba3d8a9d97dcf745d
rihib <rihib@rihib.dev>
2025-07-10 00:28:59 +0900

    refactor symmetric tree

--- go/symmetric_tree.go
+++ go/symmetric_tree.go
@@ -4,17 +4,17 @@ package main
 import "container/list"
 
 func isSymmetric(root *TreeNode) bool {
-	return isSymmetricHelper(root.Left, root.Right)
+	return areRootsSymmetric(root.Left, root.Right)
 }
 
-func isSymmetricHelper(left, right *TreeNode) bool {
+func areRootsSymmetric(left, right *TreeNode) bool {
 	if left == nil && right == nil {
 		return true
 	}
 	if left == nil || right == nil || left.Val != right.Val {
 		return false
 	}
-	return isSymmetricHelper(left.Left, right.Right) && isSymmetricHelper(left.Right, right.Left)
+	return areRootsSymmetric(left.Left, right.Right) && areRootsSymmetric(left.Right, right.Left)
 }
 
 type nodePair struct {
